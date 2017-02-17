45bc16b52bb7a4bb733dde11921e76764673293c
Fischer.json <fischer.json@gmail.com>
2017-02-17 12:17:14 +0800

    Try intermittent
    MozReview-Commit-ID: Dj0cjkz8ScM

--- browser/components/preferences/in-content/tests/browser_advanced_siteData.js
+++ browser/components/preferences/in-content/tests/browser_advanced_siteData.js
@@ -1,7 +1,7 @@
 /* Any copyright is dedicated to the Public Domain.
  * http://creativecommons.org/publicdomain/zero/1.0/ */
 
-"use strict";
+"use strict"; // Tryyyyyyy
 
 const { classes: Cc, interfaces: Ci, utils: Cu, results: Cr } = Components;
 
