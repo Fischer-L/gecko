76af9898db5685271fccd36e86638e1d96f177de
Fischer.json <fischer.json@gmail.com>
2017-08-24 17:33:06 +0800

    Bug 1372592 - Should not display non-persistent-storage sites which stores 0 byte in Preferences Site Data section, r?jaws
    MozReview-Commit-ID: BelcT0Lpmba

--- browser/components/preferences/in-content-new/tests/browser_siteData2.js
+++ browser/components/preferences/in-content-new/tests/browser_siteData2.js
@@ -437,3 +437,46 @@ add_task(async function() {
   mockSiteDataManager.unregister();
   await BrowserTestUtils.removeTab(gBrowser.selectedTab);
 });
+
+// Test not displaying sites which store 0 byte and don't have persistent storage.
+add_task(async function() {
+  await SpecialPowers.pushPrefEnv({set: [["browser.storageManager.enabled", true]]});
+  mockSiteDataManager.register(SiteDataManager);
+  mockSiteDataManager.fakeSites = [
+    {
+      usage: 0,
+      principal: Services.scriptSecurityManager
+                         .createCodebasePrincipalFromOrigin("https://account.xyz.com"),
+      persisted: true
+    },
+    {
+      usage: 0,
+      principal: Services.scriptSecurityManager
+                         .createCodebasePrincipalFromOrigin("https://shopping.xyz.com"),
+      persisted: false
+    },
+    {
+      usage: 1024,
+      principal: Services.scriptSecurityManager
+                         .createCodebasePrincipalFromOrigin("http://cinema.bar.com"),
+      persisted: true
+    },
+    {
+      usage: 1024,
+      principal: Services.scriptSecurityManager
+                         .createCodebasePrincipalFromOrigin("http://email.bar.com"),
+      persisted: false
+    },
+  ];
+  let fakeHosts = mockSiteDataManager.fakeSites.map(site => site.principal.URI.host);
+
+  let updatePromise = promiseSiteDataManagerSitesUpdated();
+  let doc = gBrowser.selectedBrowser.contentDocument;
+  await openPreferencesViaOpenPreferencesAPI("privacy", { leaveOpen: true });
+  await updatePromise;
+  await openSiteDataSettingsDialog();
+  assertSitesListed(doc, fakeHosts.filter(host => host != "shopping.xyz.com"));
+
+  mockSiteDataManager.unregister();
+  await BrowserTestUtils.removeTab(gBrowser.selectedTab);
+});
